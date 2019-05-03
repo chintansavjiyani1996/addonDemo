7ec65738ef7943df0908c7e87ae08b0d8462534d
chintansavjiyani1996 <savjiyanichintan1996@gmail.com>
2019-05-03 12:55:52 +0530

    networking update

--- Networking.js
+++ Networking.js
@@ -1,5 +1,33 @@
-export class Networking {
+export class Networking{
     static post=(url,formData)=>{
-        return fetch(url)
+        return fetch(url, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: formData,
+        }).then(Networking.handelMessage).then((response) => response.json()).catch((error)=>{
+            return Promise.reject(error)
+        })
+    };
+
+    static get=(url)=>{
+        console.log(" --->url " ,url );
+        return fetch(url, {
+            method: 'get',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        }).then(Networking.handelMessage).then((response) => response.json()).catch((error)=>{
+            return Promise.reject(error)
+        })
+    };
+
+
+    static handelMessage=(response)=>{
+        return response;
     }
+
 }
