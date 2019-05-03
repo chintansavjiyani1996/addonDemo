ba9dfe7ec153bed44dfce54467cf469c63020579
chintansavjiyani1996 <savjiyanichintan1996@gmail.com>
2019-05-03 19:08:58 +0530

    Update PlaceDetailsScreen.js
    Mobx added

--- screen/PlaceDetailsScreen.js
+++ screen/PlaceDetailsScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import {inject} from "mobx-react";
+import {Image, StyleSheet, Text, TouchableOpacity, View,ScrollView} from 'react-native';
+import {inject,observer} from "mobx-react";
+import {toJS} from "mobx";
 @inject('rootStore')
+@observer
 export default class PlaceDetailsScreen extends React.Component {
     static navigationOptions ={
         header:null
@@ -15,10 +17,14 @@ export default class PlaceDetailsScreen extends React.Component {
         );
     }
 
+    onPressGoBack=()=>{
+      this.props.navigation.goBack()
+    };
+
     renderHeader = () => {
         return (
             <View style={{flex: 0.1, flexDirection: "row",backgroundColor:"blue", alignItems: "center"}}>
-                <TouchableOpacity onPress={this.onPressGoback}>
+                <TouchableOpacity onPress={this.onPressGoBack}>
                     <Image source={require('../assets/logout.png')} style={{height:23,width:23,tintColor:"black"}}/>
                 </TouchableOpacity>
                 <Text>Details</Text>
@@ -28,16 +34,15 @@ export default class PlaceDetailsScreen extends React.Component {
     }
 
     renderContent=()=>{
-        console.log(this.props)
         return(
-            <View style={{borderWidth: 1,marginVertical: 20,marginHorizontal: 20,borderRadius:4}}>
+            <ScrollView style={{borderWidth: 1,marginVertical: 20,marginHorizontal: 20,borderRadius:4,flex:0.9}}>
                 <Text style={{fontSize:22,marginVertical:10,textAlign: "center"}}>
-                    {"Vastrapur lack"}
+                    {this.props.rootStore.dashboardStore.selectedPlace.title}
                 </Text>
-                {/*<Image source={{uri:item.img}} style={{height:200 ,width:"100%"}}/>*/}
-                {/*<Text>{item.description}*/}
-                {/*</Text>*/}
-            </View>
+                <Image source={{uri:this.props.rootStore.dashboardStore.selectedPlace.img}} style={{height:200 ,width:"100%"}}/>
+                <Text>{this.props.rootStore.dashboardStore.selectedPlace.description}
+                </Text>
+            </ScrollView>
         )
     }
 }
